2bd444361566530559b7ccc7c6763496e216b7b0
seanw <SeanCBrachvogel@gmail.com>
2023-11-27 11:44:59 -0500

    Added map to main(right now I was not sure where to display the map so to see it just change the onClick to go to the map widget instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 void main() {
@@ -284,6 +285,46 @@ class Register extends StatelessWidget {
             //const Text("Don't have an account? Sign up", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
           ],
         ),
+      ),
+    );
+  }
+}
+
+//Create a map centered on Sydney, Australia.
+class OpenMap extends StatelessWidget {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-33.86, 151.20);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Maps Sample App'),
+            backgroundColor: Colors.green[700],
+          ),
+          body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+            markers: {
+              const Marker(
+                markerId: MarkerId('Sydney'),
+                position: LatLng(-33.86, 151.20),
+                infoWindow: InfoWindow(
+                  title: "Sydney",
+                  snippet: "Capital of New South Wales",
+                ),
+              )
+            },
+          )
       ),
     );
   }
